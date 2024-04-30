1bc474765f8ced034e1f754184770617de9386a6
askaer <realaskaer@gmail.com>
2024-04-30 11:30:37 +0300

    Update client.py

--- modules/client.py
+++ modules/client.py
@@ -246,7 +246,7 @@ class Client(Logger):
                                poll_latency:int = 10, timeout:int = 360):
         try:
             if not without_gas:
-                transaction['gas'] = random.randint(1500000, 2000000)
+                transaction['gas'] = random.randint(3500000, 5000000)
         except Exception as error:
             raise RuntimeError(f'Gas calculating | {self.get_normalize_error(error)}')
 
