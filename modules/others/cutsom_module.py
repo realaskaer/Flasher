4de06b2f361974433f40ee099b35b835a4ea0775
askaer <realaskaer@gmail.com>
2024-06-05 15:58:06 +0300

    Update cutsom_module.py

--- modules/others/cutsom_module.py
+++ modules/others/cutsom_module.py
@@ -228,7 +228,7 @@ class Custom(Logger, Aggregator):
 
         dep_address = get_wallet_for_deposit(self)
 
-        _, balance, _ = await self.client.get_token_balance('TAIKO')
+        _, balance, _ = await self.client.get_token_balance('TAIKO', omnicheck=True)
         balance_in_wei = self.client.to_wei(balance)
 
         self.logger_msg(*self.client.acc_info, msg=f'Transfer {balance:.2f} TAIKO to {dep_address[:10]}...')
